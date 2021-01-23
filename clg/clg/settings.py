0c2c545341000b0b137b8ba30ea366160643e1e2
Manish Kumar <manis.kumar@gmail.com>
2021-01-23 16:05:55 +0530

    Added result app

--- clg/clg/settings.py
+++ clg/clg/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'result',
 ]
 
 MIDDLEWARE = [
-    'result.app.ResultConfig'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
