4fc886a74b8070d614c50d578078f2fabf5a7b92
harsh <rajharsh662@gmail.com>
2021-02-21 17:32:48 +0530

    first commit

--- clg/result/models.py
+++ clg/result/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=50)
+    father_name = models.CharField(max_length=50)
+    mother_name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    college = models.CharField(max_length=50)
+    registration = models.IntegerField()
+    branch = models.CharField(max_length=50)
+    batch = models.CharField(max_length=50)
+    a = models.FloatField(default=0)
+    b = models.FloatField(default=0)
+    c = models.FloatField(default=0)
+    d = models.FloatField(default=0)
+    e = models.FloatField(default=0)
+    f = models.FloatField(default=0)
+    g = models.FloatField(default=0)
+    h = models.FloatField(default=0)
+    cgpa = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
--- clg/result/views.py
+++ clg/result/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    context = {"class" : 'B.Tech', 'subject' : 'Computer Science and Engineering'}
-    return render(request,'result/index.html', context)
+    student = Student.objects.all()[0]
+    params = {"stud": student}
+    return render(request, "result/index.html", params)
